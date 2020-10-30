0d3b01b54a0470c3194a17dfb739285d61d6461d
Ben Chan <benchan777@gmail.com>
2020-10-30 15:48:03 -0700

    add deposit function

--- BankAccount.py
+++ BankAccount.py
@@ -4,3 +4,11 @@ class BankAccount:
         self.account_number = account_number
         self.routing_number = routing_number
         self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return f"Amount Deposited: ${self.balance}"
+
+
+ben = BankAccount("Ben chan", 39287362, 928162736, 0)
+print(ben.deposit(45.01))
